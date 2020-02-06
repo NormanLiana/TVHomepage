0bf60f57df23bf2c805f9d2c2410ec267215cab2
NormanLiana <liananorman@pursuit.org>
2020-02-06 15:39:58 -0500

    creates folder for controllers and holds HomePageVC

---
+++ TruViewHomePage/Controllers/HomePageVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageVC.swift
+//  TruViewHomePage
+//
+//  Created by Liana Norman on 2/6/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
