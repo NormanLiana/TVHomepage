d09c7c22adb8fcb2f16ad543c4bd344188018875
NormanLiana <liananorman@pursuit.org>
2020-02-06 15:32:13 -0500

    initial commit

---
+++ TruViewHomePage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TruViewHomePage
+//
+//  Created by Liana Norman on 2/6/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
