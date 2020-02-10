cecc8d9e81992a1f87249aa4cb92c61f14b1fd33
NormanLiana <liananorman@pursuit.org>
2020-02-10 17:58:32 -0500

    adds folder for utilities and file for card state and creates enum in file

---
+++ TruViewHomePage/Utilities/SlideCardState.swift
@@ -0,0 +1,13 @@
+//
+//  SlideCardState.swift
+//  TruViewHomePage
+//
+//  Created by Liana Norman on 2/10/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import Foundation
+
+enum SlideCardState {
+    case collapsed, halfOpen, fullOpen
+}
