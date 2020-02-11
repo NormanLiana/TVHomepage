f3d54677c93cb3efb670dbc5432e300b8bdafd9a
NormanLiana <liananorman@pursuit.org>
2020-02-11 16:52:03 -0500

    adds UI Object for a CV, adds a subview of the custom view with constraints

--- TruViewHomePage/Views/ListView.swift
+++ TruViewHomePage/Views/ListView.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class ListView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - UI Objects
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.register(ListViewCVCell.self, forCellWithReuseIdentifier: CellIdentifiers.listViewCVCell.rawValue)
+        return cv
+    }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        constrainCV()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubViews() {
+        addSubview(collectionView)
+    }
+    
+    private func addConstraints() {
+        constrainCV()
+    }
+    
+    private func constrainCV() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [collectionView.topAnchor.constraint(equalTo: topAnchor), collectionView.leadingAnchor.constraint(equalTo: leadingAnchor), collectionView.trailingAnchor.constraint(equalTo: trailingAnchor), collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)].forEach({$0.isActive = true})
+    }
+    
 }
